d38244843a920386d52f6e830212b5a8e64261b6
Gabriel C. A. Rangel <sala302b@DR302B37104.ceines.com.br>
2019-02-22 11:33:03 -0300

    Cadastrando telefone.

--- src/projetointegrador/ProjetoIntegrador.java
+++ src/projetointegrador/ProjetoIntegrador.java
@@ -4,6 +4,7 @@ public class ProjetoIntegrador {
 
     public static void main(String[] args) {
         Clientes cl = new Clientes("425.531.050-53", "Gabriel", "101", "perto do supermercado");
+        Telefones nm = new Telefones ("33157814");
         Enderecos cl_end = new Enderecos("29045-680", "Rua Limoeiro", "Bairro Concesão");
         Telefones cl_tel = new Telefones("99968724");
         Produtos prdt = new Produtos(1, "Hambúrger de bacon", 12.99);
