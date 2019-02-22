37aed1a606e8099ae653448e9c00b7fb71ae2320
Lucas Falcão Pacheco <>
2019-02-22 11:05:33 -0300

    Redução de preço

--- src/projetointegrador/ProjetoIntegrador.java
+++ src/projetointegrador/ProjetoIntegrador.java
@@ -6,7 +6,7 @@ public class ProjetoIntegrador {
         Clientes cl = new Clientes("425.531.050-53", "Gabriel", "101", "perto do supermercado");
         Enderecos cl_end = new Enderecos("29045-680", "Rua Limoeiro", "Bairro Concesão");
         Telefones cl_tel = new Telefones("99968724");
-        Produtos prdt = new Produtos(1, "Hambúrger de bacon", 12.10);
+        Produtos prdt = new Produtos(1, "Hambúrger de bacon", 12.00);
         Motoboys mot = new Motoboys("HMG-2040", "425.531.050-53", "99658412");
 
     }
