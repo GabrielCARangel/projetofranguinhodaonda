d1b2be598765e6aa1ee08d18b5b7b416321a9851
Gabriel C. A. Rangel <ansesquis@gmail.com>
2019-02-27 08:41:35 -0300

    Sistema de login.

--- src/FranguinhoDaOnda/ProjetoIntegrador.java
+++ src/FranguinhoDaOnda/ProjetoIntegrador.java
@@ -1,15 +1,13 @@
 package FranguinhoDaOnda;
 
+import FranguinhoDaOnda.view.Login;
+
 public class ProjetoIntegrador {
 
     public static void main(String[] args) {
-        Clientes cl = new Clientes("425.531.050-53", "Gabriel", "101", "perto do supermercado");
-        Telefones nm = new Telefones ("33157814");
-        Enderecos cl_end = new Enderecos("29045-680", "Rua Limoeiro", "Bairro Concesão");
-        Telefones cl_tel = new Telefones("99968724");
-        Produtos prdt = new Produtos(1, "Hambúrger de bacon", 12.99);
-        Motoboys mot = new Motoboys("HMG-2040", "425.531.050-53", "99658412");
-        System.out.println("Testando o Github.");
+        Login janela = new Login();
+        janela.setVisible(true);
+
 
     }
 }
